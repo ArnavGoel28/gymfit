8b8d7d09473ad9b53949d1ab1fc7887afef71a42
ArnavGoel28 <arnavgoel2866@gmail.com>
2026-05-01 02:41:34 +0530

    Configure Vercel deployment with @vercel/node builder

--- server.js
+++ server.js
@@ -45,7 +45,9 @@ mongoose.connect(MONGODB_URI, {
     useUnifiedTopology: true,
 }).then(() => {
     console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }).catch(err => {
     console.error('MongoDB connection error:', err);
 });
+
+// Export for Vercel
+export default app;
